1231e32c1ae8f6df1a48172d1542bb929d630952
mjz19910 <matthias291999@gmail.com>
2022-01-23 05:49:05 -0700

    add minimal cors_anywhere client using XMLHTTPRequest

--- cors_client.cjs
+++ cors_client.cjs
@@ -2,9 +2,9 @@ xml_fetch = async function(fet_url) {
 	return new Promise(function(a, r) {
 		let y = new XMLHttpRequest;
 		y.open("GET", fet_url, true);
-		y.setRequestHeader("x-requested-with", "xml_fetch");
+		y.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 		y.onreadystatechange = function(ev) {
-			console.log(y.readyState, y.status, y.response.length);
+			// console.log(y.readyState, y.status, y.response.length);
 			if(y.readyState == 4) {
 				let {response, responseType, status} = y;
 				a({xml_http_request_instance: y, response, responseType, status});
